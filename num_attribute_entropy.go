c08153d1c0b661fc817af40016b7dca6d92f8f86
GustavoMacCar <gmcmacar@gmail.com>
2021-05-17 17:44:18 -0300

    num_attribute_entropy now can successfully cal set_entropy

--- num_attribute_entropy.go
+++ num_attribute_entropy.go
@@ -8,29 +8,29 @@ import (
 	"errors"
 )
 
-func num_attribute_entropy(ds *dataset, label_column string, attribute string, threshhold float32) (float32, error) {
-
+func num_attribute_entropy(ds *dataset, label_column string, attribute string, threshhold float32) (float64, error) {
 	if !ds.columns[attribute].is_numerical {
 		return -1, errors.New("attribute is not numerical")
 	}
 
-	below := ds.filter(func(entry *line) bool { return entry.getColumn(label_column).(float32) < threshhold })
+	below := ds.filter(func(entry *line) bool { return entry.getColumn(attribute).(float32) < threshhold })
 
-	above := ds.filter(func(entry *line) bool { return entry.getColumn(label_column).(float32) >= threshhold })
+	above := ds.filter(func(entry *line) bool { return entry.getColumn(attribute).(float32) >= threshhold })
 
-	entropy_below := set_entropy(below.getColumn(label_column))
-	//entropy_below := float32(1.0)
-	entropy_above := set_entropy(above.getColumn(label_column))
-	//entropy_above := float32(1.0)
+	//entropy_below := set_entropy(below.getColumn(label_column))
+	entropy_below := set_entropy(below, label_column)
+	//entropy_above := set_entropy(above.getColumn(label_column))
+	entropy_above := set_entropy(above, label_column)
 
-	total_length := float32(len(ds.data))
+	total_length := float64(len(ds.data))
 
-	below_length := float32(len(below.data))
+	below_length := float64(len(below.data))
 
-	above_length := float32(len(above.data))
+	above_length := float64(len(above.data))
 
 	att_entropy := (entropy_below*(below_length/total_length) + entropy_above*(above_length/total_length))
 	//fmt.Println(att_entropy)
 	return att_entropy, nil
+	//return 0.0, nil
 
 }
